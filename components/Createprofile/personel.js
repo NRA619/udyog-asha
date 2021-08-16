de0aa86313faad209561e86ef0e6c95410cde503
NRA 1 <namanrohilla122@gmail.com>
2021-08-17 01:38:01 +0530

    email resolved

--- components/Createprofile/personel.js
+++ components/Createprofile/personel.js
@@ -232,6 +232,7 @@ export default function Personel({ ...data }) {
                 id="email_"
                 name="email"
                 {...register("email", {})}
+                value={email}
                 className="text-indigo-900 rounded-md p-4 shadow-lg focus:outline-none h-10 focus:ring-1 focus:ring-black"
               ></input>
             </fieldset>
