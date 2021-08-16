a5ad339e725450b3dd890609408d94cd83592ba1
NRA 1 <namanrohilla122@gmail.com>
2021-08-16 15:56:39 +0530

    import error solved

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
-import Layout from '../components/layout'
-import '../styles/globals.css'
-import 'tailwindcss/tailwind.css'
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
