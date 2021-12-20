7bb98055756e7214c1bda4fd83ee09f9a6f06b57
NRA 1 <namanrohilla122@gmail.com>
2021-12-20 12:14:04 +0530

    service_improved1

--- pages/services/service.js
+++ pages/services/service.js
@@ -17,36 +17,36 @@ export const service = () => {
 
     return (
         <div className="h-full min-h-screen w-full pt-20 px-10">
-            <div className="bg-yellow grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 h-full w-full mt-20 gap-4 mb-20">
-                <div onClick={bussiness} className="flex flex-col justify-between bg-yellow-400 rounded-lg h-72  shadow-md">
-                    <span className="text-center pt-10 px-5 text-black">
+            <div className="bg-yellow grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 h-full w-full mt-20 gap-4">
+                <div onClick={bussiness} className="flex flex-col justify-between bg-bsupport bg-cover bg-opacity-40 rounded-lg h-72  shadow-md">
+                    <span className="text-center pt-10 px-5 rounded-t-lg text-black bg-yellow-100 bg-opacity-50 h-full ">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry.         
                     </span>
-                    <button className="bg-yellow-100 hover:bg-black hover:text-white rounded-b-lg text-gray-800 bg-opacity-60 shadow-xl py-4">
+                    <button className="bg-yellow-300  hover:bg-black hover:text-white font-medium rounded-b-lg text-gray-800 bg-opacity-60 shadow-xl py-4">
                         Bussiness Support
                     </button>
                 </div>
-                <div onClick={registration} className="flex flex-col justify-between bg-green-400 rounded-lg h-72  shadow-md">
-                    <span className="text-center pt-10 px-5 text-black">
+                <div onClick={registration} className="flex flex-col justify-between bg-registration bg-cover rounded-lg h-72  shadow-md">
+                    <span className="text-center pt-10 px-5 rounded-t-lg text-black bg-green-100 bg-opacity-60 h-full">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry.         
                     </span>
-                    <button className="bg-green-100 hover:bg-black hover:text-white rounded-b-lg text-gray-800 bg-opacity-60 shadow-xl py-4">
+                    <button className="bg-green-300 rounded-b-lg hover:bg-black font-medium hover:text-white text-gray-800 bg-opacity-60 shadow-xl py-4">
                         Registration
                     </button>
                 </div>
-                <div onClick={printmedia} className="flex flex-col justify-between bg-blue-400 rounded-lg h-72  shadow-md">
-                    <span className="text-center pt-10 px-5 text-black">
+                <div onClick={printmedia} className="flex flex-col justify-between bg-pmedia bg-cover rounded-lg h-72  shadow-md">
+                    <span className="text-center pt-10 px-5 rounded-t-lg text-black  bg-blue-100 bg-opacity-60 h-full">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry.         
                     </span>
-                    <button className="bg-blue-100 hover:bg-black hover:text-white rounded-b-lg text-gray-800 bg-opacity-60 shadow-xl py-4">
+                    <button className="bg-blue-300 rounded-b-lg hover:bg-black font-medium hover:text-white text-gray-800 bg-opacity-60 shadow-xl py-4">
                         Print Media
                     </button>
                 </div>
-                <div onClick={marketing} className="flex flex-col justify-between bg-red-400 rounded-lg h-72  shadow-md">
-                    <span className="text-center pt-10 px-5 text-black">
+                <div onClick={marketing} className="flex flex-col justify-between bg-marketing bg-cover rounded-lg h-72  shadow-md">
+                    <span className="text-center pt-10 px-5 rounded-t-lg  text-black bg-red-100 bg-opacity-60 h-full">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry.         
                     </span>
-                    <button className="bg-red-100 hover:bg-black hover:text-white rounded-b-lg text-gray-800 bg-opacity-60 shadow-xl py-4">
+                    <button className="bg-red-300 rounded-b-lg hover:bg-black font-medium hover:text-white text-gray-800 bg-opacity-60 shadow-xl py-4">
                         Social Media Marketing
                     </button>
                 </div>
