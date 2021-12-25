68daed26a8c0359b5f814f1f54845d48377207aa
NRA 1 <namanrohilla122@gmail.com>
2021-12-25 20:58:17 +0530

    query mail1

--- components/Footer.js
+++ components/Footer.js
@@ -4,9 +4,36 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import CopyrightRoundedIcon from '@material-ui/icons/CopyrightRounded';
 import Link from 'next/link';
-import TouchAppRoundedIcon from '@material-ui/icons/TouchAppRounded';
+import React, { useState, useRef, useEffect } from "react";
+import { Controller, useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function Footer() {
+    const router = useRouter();
+  const {
+    control,
+    register,
+    formState: { errors },
+    handleSubmit,
+    watch,
+  } = useForm();
+  async function loginform(values) {
+      const name = values.name;
+      const email = values.email;
+      const query = values.query;
+      const res = await axios.post("https://murmuring-eyrie-62394.herokuapp.com/user/sendmail", {
+        name:name,
+        email:email,
+        query:query,
+      });
+
+      if(res.data.done === true) {
+          alert("Mail send successfully");
+      }else {
+          alert("Oops! Something went wrong");
+      }
+  }
     return (
         <div className="bg-black p-6 text-md font-semibold divide-y divide-white md:divide-y-0 flex-col md:flex md:flex-row md:justify-between shadow-xl">   
            
@@ -55,31 +82,48 @@ export default function Footer() {
                     Contact us
                 </span>
 
-                <form className="mt-4">
+                <form onSubmit={handleSubmit(loginform)} className="mt-4">
                     <div className="mb-2">
-                        <label className="block font-semibold text-white ">
+                        <label htmlFor="name_" className="block font-semibold text-white ">
                             Name
                         </label>
-                        <input className="appearance-none border rounded md:w-96 w-full p-2 font-semibold focus:outline-none focus:shadow-inner " ></input>
+                        <input id="name_"
+                          // register field
+                          {...register("name", {
+                            // validations
+                            required: true,
+                          })} name="name" className="appearance-none border rounded md:w-96 w-full p-2 font-semibold focus:outline-none focus:shadow-inner " ></input>
                     </div>
 
                     <div className="mb-2">
-                        <label className="block font-semibold text-white ">
+                        <label htmlFor="email_" className="block font-semibold text-white ">
                             e-mail
                         </label>
-                        <input type="email" className="appearance-none border rounded w-full p-2 font-semibold"></input>
+                        <input type="email" id="email_"
+                          // register field
+                          {...register("email", {
+                            // validations
+                            required: true,
+                          })}
+                          name="email" className="appearance-none border rounded w-full p-2 font-semibold"></input>
                     </div>
 
 
                     <div className="mb-2">
-                        <label className="block font-semibold text-white ">
-                            Phone
+                        <label htmlFor="query_" className="block font-semibold text-white ">
+                            query
                         </label>
-                        <input type="" className="appearance-none border rounded w-full p-2 font-semibold"></input>
+                        <input type="text" id="query_"
+                          // register field
+                          {...register("query", {
+                            // validations
+                            required: true,
+                          })}
+                          name="query" className="appearance-none border rounded w-full p-2 font-semibold"></input>
                     </div>
 
                     <div className="md:flex md:flex-col md:items-center">
-                        <button className="md:w-1/4 w-3/4 hover:cursor-pointer py-2 text-lg font-extrabold bg-white mt-2 text-blue-500">Submit</button>
+                        <button type="submit" className="md:w-1/4 w-3/4 hover:cursor-pointer py-2 text-lg font-extrabold bg-white mt-2 text-blue-500">Submit</button>
                     </div>
                 </form>
             </div>
