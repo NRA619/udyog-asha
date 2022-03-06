4ae8141231542226bfc4c3cd04545b9c08719c41
NRA 1 <namanrohilla122@gmail.com>
2022-03-06 16:59:51 +0530

    full_screen solved

--- pages/training/lectures.js
+++ pages/training/lectures.js
@@ -70,7 +70,7 @@ const Lecture = () => {
               src={vedio_link}
               width="100%"
               height="100%"
-              allowfullscreen="true"
+              allowFullScreen="true"
               allow="autoplay"
               className=""
             />
