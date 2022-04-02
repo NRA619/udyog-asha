1754dd2af5da864da186ec93644c4263bb85901f
NRA 1 <namanrohilla122@gmail.com>
2022-04-03 01:23:05 +0530

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import Layout from "../components/Layout";
 import "../styles/globals.css";
 import { CookiesProvider } from "react-cookie";
 import { useEffect } from 'react';
-import { pageview } from  '../gtag'
+
 
 export default function MyApp({ Component, pageProps, router }) {
 
