7a54877896e1aa9abbdfbf14a3e82c2e2f4d2085
NRA 1 <namanrohilla122@gmail.com>
2022-04-03 01:06:21 +0530

    analytics

--- gtag.js
+++ gtag.js
@@ -1,13 +1,13 @@
-export const GA_TRACKING_ID = 'UA-224767283-1';
+export const GA_TRACKING_ID = 'UA-224759180-1';
 
-export default function pageview(url, title) {
+export const pageview = (url, title) => {
   window.gtag('config', GA_TRACKING_ID, {
     page_location: url,
     page_title: title,
   });
 };
 
-export default function event({ action, category, label, value }) {
+export const event = ({ action, category, label, value }) => {
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
