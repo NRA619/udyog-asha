5683c5d6888cab3d79701a253a42a2a6febca6ae
NRA 1 <namanrohilla122@gmail.com>
2022-04-03 00:58:14 +0530

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import { CookiesProvider } from "react-cookie";
+import { useEffect } from 'react';
+import { pageview } from  './gtag'
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps, router }) {
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      pageview(url, document.title);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
   return (
     
     <Layout>
